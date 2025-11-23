26c9a1aa8c8a7ca166eeb71111d4d25c6322f1cf
luckylark2000 <3286481191@qq.com>
2025-11-23 23:23:06 +0800

    add(docs,src):python变量声明学习

---
+++ src/quick-start-python/var.py
@@ -0,0 +1,25 @@
+"""_summary_
+学习 Python 基础: 变量、数据类型、列表、字典、元组
+对应文档:docs\quick-start-python\basic-grammar\变量和数据类型.md
+"""
+
+# 变量声明
+num = 3 # 不用用类似 let, const 之类的，python 自动识别
+
+## 带类型的（3.9版本开始支持）,类型提示需要安装pyright插件
+a:bool = True
+
+print(num)
+print(a)
+
+# 数据类型
+i=3
+b = 1.2
+c = True
+
+print(type(i)) #<class 'int'>
+print(type(b)) #<class 'float'>
+print(type(c)) #<class 'bool'>
+#
+
+
